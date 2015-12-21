8cae3224a1ad57b235de3160a909b7d235aaa920
Luming Wu <reallumingwu@gmail.com>
2015-12-21 10:00:16 -0500

    Added weighted vertex

--- Graph/src/Vertex/WeightedVertex.java
+++ Graph/src/Vertex/WeightedVertex.java
@@ -4,15 +4,16 @@ import Relation.Adjacency;
 import java.util.ArrayList;
 import java.util.LinkedList;
 
-public class Vertex implements Comparable<Vertex>{
+public class WeightedVertex implements Comparable<WeightedVertex>{
     
+    public double weight = Double.POSITIVE_INFINITY;
     public String name;
     public Vertex predecesspr = null;
     public Vertex successor = null;
     public ArrayList<Adjacency> staticAdjacencies = new ArrayList<Adjacency>();
     public LinkedList<Adjacency> activeAdjacencies = new LinkedList<Adjacency>();
-    
-    public Vertex(String id){
+
+    public WeightedVertex(String id) {
         name = id;
     }
     
@@ -20,9 +21,9 @@ public class Vertex implements Comparable<Vertex>{
     public String toString(){
         return name;
     }
-    
+
     @Override
-    public int compareTo(Vertex o) {
+    public int compareTo(WeightedVertex o) {
         return name.compareTo(o.name);
     }
 }
